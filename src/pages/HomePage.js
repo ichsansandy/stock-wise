d90ef2ecaba5367a5f961c138910214da323b410
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-07-22 10:36:06 +0700

    Add landing page text

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -51,17 +51,42 @@ function HomePage() {
 
   return (
     <>
-      <div className={`h-[40vh] bg-primary ${theme}`}>Container Landing Image</div>
-      <div className={`flex justify-center lg:pt-4 items-center z-10 sticky top-[78.6px] ${isScrolled ? 'lg:-translate-y-11 ' : 'backdrop-blur-md'}`}>
-        <div className={`h-[10vh] w-full drop-shadow-2xl max-w-filter flex gap-2  items-center lg:rounded-xl px-7 py-4 lg:w-[80%] bg-gradient-to-r from-secondary from-40% to-primary lg:mb-3 ${theme}`}>
-          <input className="px-4 py-2 w-[80%] placeholder:text-xs sm:placeholder:text-base rounded-xl" onChange={(e) => setNameInput(e.target.value)} value={nameInput} type="text" placeholder="Type company name or stock symbol you want to find " />
+      <div className={`h-[40vh] lg:h-[50vh] bg-primary flex justify-center ${theme}`}>
+        <div
+          className={`${theme} w-full max-w-wrap text-textClr flex items-center px-1 justify-center `}
+        >
+          <h1 className="text-3xl lg:text-5xl leading-relaxed lg:leading-[1.6]">
+            <span className="font-bold">Uncover</span>
+            <span className="bg-textClr m-2 rounded-2xl pb-1 px-2 lg:pb-3"><img className="inline w-[80px] lg:w-[120px] " alt="nasdaq-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/NASDAQ_Logo.svg/1280px-NASDAQ_Logo.svg.png" /></span>
+            <br />
+            <span className="text-6xl lg:text-8xl font-extrabold underline decoration-accent ">Wisely.</span>
+          </h1>
+        </div>
+      </div>
+      <div
+        className={`flex justify-center lg:pt-4 items-center z-10 sticky top-[78.6px] ${
+          isScrolled ? 'lg:-translate-y-11 ' : 'backdrop-blur-md'
+        }`}
+      >
+        <div
+          className={`h-[10vh] w-full drop-shadow-2xl max-w-filter flex gap-2  items-center lg:rounded-xl px-7 py-4 lg:w-[80%] bg-gradient-to-r from-secondary from-40% to-primary lg:mb-3 ${theme}`}
+        >
+          <input
+            className="px-4 py-2 w-[80%] placeholder:text-xs sm:placeholder:text-base rounded-xl"
+            onChange={(e) => setNameInput(e.target.value)}
+            value={nameInput}
+            type="text"
+            placeholder="Type company name or stock symbol you want to find "
+          />
           <Dropdown
             menu={filterProps}
             overlayClassName="h-[200px] overflow-scroll"
             placement="bottomRight"
             trigger={['click']}
           >
-            <div className={`${theme} cursor-pointer w-[20%] bg-white justify-center flex items-center gap-2 border-accent border-2 p-2 rounded-xl hover:text-textClr hover:bg-accent transition`}>
+            <div
+              className={`${theme} cursor-pointer w-[20%] bg-white justify-center flex items-center gap-2 border-accent border-2 p-2 rounded-xl hover:text-textClr hover:bg-accent transition`}
+            >
               <Filter />
               <div className="hidden md:block">Sector</div>
             </div>
@@ -70,7 +95,18 @@ function HomePage() {
       </div>
       <div className="grid lg:place-content-center mb-7">
         <div className="max-w-wrap grid grid-cols-2 sm:grid-cols-3 lg:flex lg:flex-wrap lg:justify-center lg:gap-3 [&>*:nth-child(4n+0)]:max-sm:bg-secondary [&>*:nth-child(4n+1)]:max-sm:bg-secondary sm:[&>*:nth-child(2n)]:bg-secondary">
-          {status === 'pending' ? <div>Loading ...</div> : stockData.map((data) => <StockCard data={data} key={data.cik} nameInput={nameInput} selectedSector={selectedSector} />)}
+          {status === 'pending' ? (
+            <div>Loading ...</div>
+          ) : (
+            stockData.map((data) => (
+              <StockCard
+                data={data}
+                key={data.cik}
+                nameInput={nameInput}
+                selectedSector={selectedSector}
+              />
+            ))
+          )}
         </div>
       </div>
     </>
