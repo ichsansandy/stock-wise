b05a2247999e45715dd529565f644cd5a8f8577d
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-07-22 15:58:00 +0700

    redesign about me

--- src/pages/AboutmePage.js
+++ src/pages/AboutmePage.js
@@ -7,32 +7,55 @@ function AboutmePage() {
 
   return (
     <div className="flex justify-center">
-      <div className="max-w-wrap w-full flex flex-col gap-2 p-4 items-center">
-        <div className="text-center">
-          If you have any question or want to say &quot;Hello&quot;?
-          <br />
-          You can reach me here
-        </div>
+      <div className="max-w-wrap w-full flex flex-wrap gap-2 p-4 lg:justify-center">
         <div
-          className={`${theme} flex flex-col items-center justify-center w-[50%] border-2 border-primary p-3 rounded-xl mt-3 gap-2 max-w-[150px] lg:max-w-[250px] lg:text-xl lg:p-7 lg:gap-5`}
+          className={`${theme} border-2 p-3 rounded-xl border-primary text-left w-[100%] text-6xl lg:text-8xl lg:p-6 font-bold bg-accent text-textClr`}
         >
-          <img
-            alt="Ichsan profile"
-            className={`${theme} w-20 lg:w-32 aspect-square rounded-full border-accent border-2 overflow-hidden`}
-            src="https://avatars.githubusercontent.com/u/108980933?v=4"
-          />
-          <h1>Ichsan Sandy</h1>
-          <div className={`${theme} flex gap-2 lg:gap-6 `}>
-            <a className="hover:text-primary lg:scale-150 " href="http://github.com/ichsansandy" target="_blank" rel="noreferrer">
-              <Github />
-            </a>
-            <a className="hover:text-primary lg:scale-150" href="https://www.linkedin.com/in/ichsans/" target="_blank" rel="noreferrer">
-              <Linkedin />
-            </a>
-            <a className="hover:text-primary lg:scale-150" href="https://www.instagram.com/ichsans__/" target="_blank" rel="noreferrer">
-              <Instagram />
-            </a>
-          </div>
+          <span>FORGET. </span>
+          <br />
+          <span>ABOUT. </span>
+          <br />
+          <span>ME. </span>
+          <br />
+          <span>NOT. </span>
+          <br />
+        </div>
+
+        <div className={`${theme} flex gap-2 lg:gap-6 lg:p-5  text-2xl uppercase font-bold border-2 border-primary bg-white p-3 rounded-xl mt-3 text-primary`}>
+          Ichsan Sandy
+        </div>
+
+        <div
+          className={`${theme} flex items-center gap-2 lg:gap-6 lg:p-5 border-2 border-primary bg-accent p-3 rounded-xl mt-3 `}
+        >
+          <a
+            className="hover:text-primary lg:scale-150 "
+            href="http://github.com/ichsansandy"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Github className={`${theme} text-textClr`} />
+          </a>
+          <a
+            className="hover:text-primary lg:scale-150"
+            href="https://www.linkedin.com/in/ichsans/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Linkedin className={`${theme} text-textClr`} />
+          </a>
+          <a
+            className="hover:text-primary lg:scale-150"
+            href="https://www.instagram.com/ichsans__/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Instagram className={`${theme} text-textClr`} />
+          </a>
+        </div>
+
+        <div className={`${theme} flex gap-2 lg:gap-6 lg:p-5 text-xl uppercase font-bold border-2 border-primary bg-primary p-3 rounded-xl mt-3 text-textClr`}>
+          2023
         </div>
       </div>
     </div>
